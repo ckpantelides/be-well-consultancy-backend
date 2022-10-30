8efe915bb4258ac64e0f17efca4f03f5936559e9
Christos Kyriacos Pantelides <ckpantelides@gmail.com>
2022-10-30 19:53:47 +0000

    Send auto acknowledgment email

--- server.js
+++ server.js
@@ -6,8 +6,13 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({ extended: true })); // support urlencoded bodies
- 
-const whitelistContactForm = ["https://bewellconsultancy.com", "https://bewellconsultancy.com/#contact", "https://bewellconsultancy.com/founder", "https://bewellconsultancy.com/therapy"];
+
+const whitelistContactForm = [
+  "https://bewellconsultancy.com",
+  "https://bewellconsultancy.com/#contact",
+  "https://bewellconsultancy.com/founder",
+  "https://bewellconsultancy.com/therapy",
+];
 const corsContactForm = {
   origin: function (origin, callback) {
     if (whitelistContactForm.indexOf(origin) !== -1) {
@@ -27,7 +32,7 @@ const corsContactForm = {
 app.use(express.static("."));
 
 // Pre-flight requests for api routes from whitelist only
-app.options("/contact-form", cors(corsContactForm))
+app.options("/contact-form", cors(corsContactForm));
 
 app.get("/", cors(), (req, res) => res.send("Be Well :)"));
 
@@ -35,33 +40,51 @@ app.get("/create-payment-intent", cors(), (req, res) =>
   res.send("Create payment intent")
 );
 
-// Webhook route confirms with Stripe that a payment intent succeeded
 app.post(
   "/contact-form",
- [cors(corsContactForm), express.json()],
- (request, response) => {
-   const contact = request.body;
-   console.log(contact)
+  [cors(corsContactForm), express.json()],
+  (request, response) => {
+    const contact = request.body;
 
-   const sgMail = require("@sendgrid/mail");
+    const sgMail = require("@sendgrid/mail");
 
-   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-   const msg = {
-     to: [process.env.FROM_CONTACT_EMAIL], 
-     from: process.env.FROM_CONTACT_EMAIL,
-     subject: 'Contact form message from Be Well Consultancy',
-     text: `${contact.name} at ${contact.email} says: ${contact.message}`,
-     html: `${contact.name} at ${contact.email} says:<br /><br />${contact.message}`
-   };
-   sgMail
-     .send(msg)
-     .then(() => {
-       console.log("Email sent");
-     })
-     .catch((error) => {
-       console.error(error);
-     });
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+    // Send contact message to Be Well Consultancy
+    const msg = {
+      to: [process.env.FROM_CONTACT_EMAIL],
+      from: process.env.FROM_CONTACT_EMAIL,
+      subject: "Contact form message from Be Well Consultancy",
+      text: `${contact.name} at ${contact.email} says: ${contact.message}`,
+      html: `${contact.name} at ${contact.email} says:<br /><br />${contact.message}`,
+    };
+    sgMail
+      .send(msg)
+      .then(() => {
+        console.log("Email sent");
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+
     response.sendStatus(200);
+
+    // Send auto acknowledgment email to contact
+    const autoAcknowledgmentEmail = {
+      to: [contact.email],
+      bcc: [process.env.FROM_CONTACT_EMAIL],
+      from: process.env.FROM_CONTACT_EMAIL,
+      templateId: "d-bd05b929be81420abd481d78988e1f51",
+    };
+
+    sgMail
+      .send(autoAcknowledgmentEmail)
+      .then(() => {
+        console.log("Auto acknowledgement email sent");
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }
 );
 
